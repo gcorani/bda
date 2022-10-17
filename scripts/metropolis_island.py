e34db45f6065d6edc3ee1f035f8c80ca05e77b7f
gcorani <gcorani@users.noreply.github.com>
2022-10-17 15:50:50 +0200

    update programming proportion

--- scripts/metropolis_island.py
+++ scripts/metropolis_island.py
@@ -8,7 +8,9 @@ Created on Mon May  2 20:47:09 2022
 import numpy as np
 import pandas as pd
 
-# decides the next island to be visited
+
+
+# decides whether to move to the proposed island
 def next_position(current,proposal):
     next_vis = current
     if proposal>current:
@@ -24,18 +26,21 @@ start = 3
 samples = 50000
 position = np.ones([samples,1])
 position[0] = start
+
 for i in range(1,samples):
-    # proposal = right
+    # proposal = right, remaining within the allowed bounds
     proposal = min(position[i-1]+1,7)
-    if  np.random.uniform()<0.5: #debug
-        # proposal = left
+
+    #with probability 50%,  proposal is changed to left (negative values are forbidden)
+    if  np.random.uniform()<0.5: 
         proposal = max(position[i-1]-1,1)
+    
+    #this array is the trace of the sampled values    
     position[i] = next_position(position[i-1],proposal)
     
     
 df = pd.DataFrame(position, columns=["position"])
 #contingency table
-a = pd.crosstab(index=df['position'],columns=df['position'], margins=True)["All"]
-freqs = a[0:7]
+freqs = pd.crosstab(index=df['position'],columns=df['position'], margins=True)["All"][0:7]
 empirical_p = freqs / sum(freqs)
 true_p = range(1,8)/np.sum(range(1,8))
