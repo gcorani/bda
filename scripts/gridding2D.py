2e724457761609d5fe67c7c69e5dfa6de62f58ff
gcorani <gcorani@users.noreply.github.com>
2022-10-19 14:25:41 +0200

    updated prob programming

--- scripts/gridding2D.py
+++ scripts/gridding2D.py
@@ -18,10 +18,13 @@ import matplotlib.pyplot as plt
 
 #we consider a grid of +- 6 prior sigma, hence +-30.
 #to keep the computation fast, we use only 80 points.
-mu = np.linspace(145, 205, 80)
+mu_len = 80
+mu = np.linspace(145, 205, mu_len)
 
 #the sigma has a broad range (see slides: 75% pctile is 40; max is 128 and we use more points)
-sigma = np.linspace(0, 100, 120)
+#the value of 0 is not feasible.
+#we would better have a non-uniform grid
+sigma = np.linspace(0.01, 100, 120)
 
 post_density = np.zeros( ( len(mu),len(sigma) ) ) 
 
@@ -35,22 +38,39 @@ for row, current_mu in enumerate(mu):
     
     #assuming independence of the y_i, the lik terms multiply
     lik   = norm.pdf(168, loc=current_mu, scale=current_sigma)
-    #lik   = lik * norm.pdf(178, loc=current_mu, scale=current_sigma)
+    lik   = lik * norm.pdf(178, loc=current_mu, scale=current_sigma)
     
     #unnormalized density
     post_density[row,col] = prior * lik
 
 #numbers are small and numerically problem can arise. 
 #use the log to have a more robust computation
-post_density = post_density/sum(post_density)
-plt.matshow(post_density)
-plt.xticks(mu)
-plt.yticks(sigma)
+
+post_density = post_density/np.sum(post_density)
+
+#for very small values of sigma, the posterior mean
+#equals the  mean of the data.
+#otherwise, it is closer  to the prior mean as we only have 
+#two observations.
+plt.figure(figsize=(10, 3))
+
+#for visualization, it is better 
+#selecting the subregion where there is more density.
+#plt.matshow(post_density)
+plt.matshow(post_density[30:50,0:40])
+plt.title("Joint")
 plt.show()
 
-#posterior marginal of mu
-post_mu = np.sum(post_density, axis=1)
-post_sigma = np.sum(post_density, axis=0)
 
+#posterior marginal of mu
+plt.subplot(1, 2, 1)
+post_mu = np.nansum(post_density, axis=1)
 plt.plot(mu,post_mu)
+plt.title("Post mean")
+
+#posterior marginal of sigma
+post_sigma = np.sum(post_density, axis=0)
+plt.subplot(1, 2, 2)
+plt.plot(sigma,post_sigma)
+plt.title("Post sigma")
 plt.show()
